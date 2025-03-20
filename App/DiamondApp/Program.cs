d24fe0d2d22ef841f6b72607968593ea033ee408
Codebrudi <codebrudi@pm.me>
2025-03-20 18:26:41 +0100

    [patch] clean up

--- App/DiamondApp/Program.cs
+++ App/DiamondApp/Program.cs
@@ -1,6 +1,6 @@
+using Brudibytes.Diamond.App.DiamondApp;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using DiamondApp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
