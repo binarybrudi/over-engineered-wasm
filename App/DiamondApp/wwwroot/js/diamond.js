feca5a78890f80de0d663ba1cbe1474daad760b6
Codebrudi <codebrudi@pm.me>
2025-03-21 20:59:07 +0100

    [patch] manual blazor start (#1)

--- App/DiamondApp/wwwroot/js/diamond.js
+++ App/DiamondApp/wwwroot/js/diamond.js
@@ -9,11 +9,3 @@ document.addEventListener('DOMContentLoaded', () => {
         rainContainer.appendChild(raindrop);
     }
 });
-
-Blazor.start().then(() => {
-    const splash_screen = document.getElementById('splash_screen');
-    splash_screen.classList.add('fade-out');
-    setTimeout(() => {
-        splash_screen.style.display = 'none'; /* Hide after fade-out */
-    }, 500); /* Matches fadeOut animation duration */
-});
