44c1f0ca272cdeac98f4617e60dcee8f64a14d4d
business-brudi <business-brudi@users.noreply.github.com>
2025-03-21 19:24:17 +0000

    deploy: b3112ba04921293cb77373db1081d0fe5c7e8f80

---
+++ js/diamond.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const rainContainer = document.querySelector('.rain_container');
+    for (let i = 0; i < 30; i++) {
+        const raindrop = document.createElement('div');
+        raindrop.classList.add('raindrop');
+        raindrop.style.left = `${Math.random() * 100}%`; /* Random horizontal position */
+        raindrop.style.animationDuration = `${1 + Math.random() * 2}s`; /* 1s to 3s */
+        raindrop.style.animationDelay = `${Math.random() * 2}s`; /* 0s to 2s delay */
+        rainContainer.appendChild(raindrop);
+    }
+});
+
+Blazor.start().then(() => {
+    const loading = document.getElementById('loading');
+    loading.classList.add('fade-out');
+    setTimeout(() => {
+        loading.style.display = 'none'; /* Hide after fade-out */
+    }, 500); /* Matches fadeOut animation duration */
+});
