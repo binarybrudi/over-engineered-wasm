05735406efc0e2a27a885e2f43f79cc618182fdd
Codebrudi <codebrudi@pm.me>
2025-03-21 21:11:11 +0100

    [patch] splash screen minimum duration 3s (#1)

--- App/DiamondApp/wwwroot/js/blazor.js
+++ App/DiamondApp/wwwroot/js/blazor.js
@@ -1,9 +1,23 @@
+const startTime = new Date().now();
+const minimum_splash_screen_time = 3000;
+
 Blazor.start().then(() => {
-    const splash_screen = document.getElementById('splash_screen');
-    splash_screen.classList.add('fade-out');
-    setTimeout(() => {
-        splash_screen.style.display = 'none'; /* Hide after fade-out */
-    }, 500); /* Matches fadeOut animation duration */
+    const elapsedTime = Date.now() - startTime;
+    const remainingTime = minimum_splash_screen_time - elapsedTime;
+    
+    const fadeOut = () => {
+        const splash_screen = document.getElementById('splash_screen');
+        splash_screen.classList.add('fade-out');
+        setTimeout(() => {
+            splash_screen.style.display = 'none'; /* Hide after fade-out */
+        }, 500); /* Matches fadeOut animation duration */
+    };
+    
+    if (remainingTime > 0) {
+        setTimeout(fadeOut, remainingTime);
+    } else {
+        fadeOut();
+    }
 }).catch(error => {
     console.error('Blazor failed to start:', error);
 });
