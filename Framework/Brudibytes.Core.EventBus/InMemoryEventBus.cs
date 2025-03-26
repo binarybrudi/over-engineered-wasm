f2856d9011af3ed8b609669c7e12f1d36f29a4bf
Codebrudi <codebrudi@pm.me>
2025-03-27 00:04:40 +0100

    [minor] add in memory event bus (#5)

---
+++ Framework/Brudibytes.Core.EventBus/InMemoryEventBus.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Concurrent;
+using Brudibytes.Core.EventBus.Contract;
+
+namespace Brudibytes.Core.EventBus;
+
+public class InMemoryEventBus : IEventBus
+{
+    private readonly ConcurrentDictionary<Type, List<object>> _subscribers = new();
+    
+    public async Task PublishAsync<TEventMessage>(TEventMessage eventMessage)
+        where TEventMessage : class, IEventMessage
+    {
+        var messageType = typeof(TEventMessage);
+            
+        if (_subscribers.TryGetValue(messageType, out var handlers))
+        {
+            var typedHandlers = handlers.OfType<IEventMessageHandler<TEventMessage>>().ToArray();
+            foreach (var handler in typedHandlers)
+            {
+                await handler.HandleAsync(eventMessage);
+            }
+        }
+    }
+
+    public void Subscribe<TEventMessage>(IEventMessageHandler<TEventMessage> handler)
+        where TEventMessage : class, IEventMessage
+    {
+        var messageType = typeof(TEventMessage);
+        _subscribers.AddOrUpdate(
+            messageType,
+            [handler],
+            (key, existingList) =>
+            {
+                existingList.Add(handler);
+                return existingList;
+            }
+        );
+    }
+}
